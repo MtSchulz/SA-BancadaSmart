280c1e1bb51b3581db9a6d7870fa5d7248a4558f
Matheus Schulz D Santos <130805275+MtSchulz@users.noreply.github.com>
2025-05-14 21:44:31 -0300

    Restart
    Recomeçando o projeto por erros desconhecidos

--- BancadaSmart/loja-pedido/src/main/java/com/exemplo/loja_pedido/LojaPedidoApplication.java
+++ BancadaSmart/loja-pedido/src/main/java/com/exemplo/loja_pedido/LojaPedidoApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LojaPedidoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LojaPedidoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(LojaPedidoApplication.class, args);
+	}
 
 }
-//teste
