5f8f6012bb0d4e3552460eb4e0db3cd3fd469da6
WilianCardoso <165498161+WilianCardoso@users.noreply.github.com>
2025-06-02 21:00:20 -0300

    implementação de novos arquivos
    Classe Lamina e LaminaRepository

---
+++ clpmonitor/src/main/java/com/clpmonitor/clpmonitor/Repository/LaminaRepository.java
@@ -0,0 +1,8 @@
+package com.clpmonitor.clpmonitor.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.clpmonitor.clpmonitor.Model.Lamina;
+
+public interface LaminaRepository extends JpaRepository<Lamina, Long> {
+}
