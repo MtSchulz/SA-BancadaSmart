cd1d13a84f4d10a152aa1c70aab87921e36f06d5
Matheus Schulz Delgado <130805275+MtSchulz@users.noreply.github.com>
2025-06-02 21:00:46 -0300

    Erro ajustado

--- clpmonitor/src/main/java/com/clpmonitor/clpmonitor/Model/Block.java
+++ clpmonitor/src/main/java/com/clpmonitor/clpmonitor/Model/Block.java
@@ -1,56 +1,52 @@
 package com.clpmonitor.clpmonitor.Model;
 
-import com.mysql.cj.x.protobuf.MysqlxCrud.Order;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "block")
 public class Block {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    
-    private Integer position;
-    private Integer color;
-    
+    private Long id;
+
+    private int position;
+
+    private int color;
+
     @ManyToOne
-    @JoinColumn(name = "storage_id")
+    @JoinColumn(name = "StorageId")
     private Storage storage;
-    
+
     @ManyToOne
     @JoinColumn(name = "production_order_id")
-    private Order productionOrder;
+    private Orders productionOrder;
 
-    
     // getters e setters
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public Integer getPosition() {
+    public int getPosition() {
         return position;
     }
 
-    public void setPosition(Integer position) {
+    public void setPosition(int position) {
         this.position = position;
     }
 
-    public Integer getColor() {
+    public int getColor() {
         return color;
     }
 
-    public void setColor(Integer color) {
+    public void setColor(int color) {
         this.color = color;
     }
 
@@ -62,11 +58,11 @@ public class Block {
         this.storage = storage;
     }
 
-    public Order getProductionOrder() {
+    public Orders getProductionOrder() {
         return productionOrder;
     }
 
-    public void setProductionOrder(Order productionOrder) {
+    public void setProductionOrder(Orders productionOrder) {
         this.productionOrder = productionOrder;
-    }    
+    }
 }
