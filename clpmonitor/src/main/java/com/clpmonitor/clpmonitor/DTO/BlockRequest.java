bcc8189e8f852a5f6e2f790585a4447ff5dd9ae8
WilianCardoso <165498161+WilianCardoso@users.noreply.github.com>
2025-06-09 18:58:21 -0300

    Erros corrigidos

---
+++ clpmonitor/src/main/java/com/clpmonitor/clpmonitor/DTO/BlockRequest.java
@@ -0,0 +1,68 @@
+package com.clpmonitor.clpmonitor.DTO;
+
+public class BlockRequest {
+
+    private int color;
+    private String l1Color;
+    private String l2Color;
+    private String l3Color;
+    private String l1Pattern;
+    private String l2Pattern;
+    private String l3Pattern;
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public String getL1Color() {
+        return l1Color;
+    }
+
+    public void setL1Color(String l1Color) {
+        this.l1Color = l1Color;
+    }
+
+    public String getL2Color() {
+        return l2Color;
+    }
+
+    public void setL2Color(String l2Color) {
+        this.l2Color = l2Color;
+    }
+
+    public String getL3Color() {
+        return l3Color;
+    }
+
+    public void setL3Color(String l3Color) {
+        this.l3Color = l3Color;
+    }
+
+    public String getL1Pattern() {
+        return l1Pattern;
+    }
+
+    public void setL1Pattern(String l1Pattern) {
+        this.l1Pattern = l1Pattern;
+    }
+
+    public String getL2Pattern() {
+        return l2Pattern;
+    }
+
+    public void setL2Pattern(String l2Pattern) {
+        this.l2Pattern = l2Pattern;
+    }
+
+    public String getL3Pattern() {
+        return l3Pattern;
+    }
+
+    public void setL3Pattern(String l3Pattern) {
+        this.l3Pattern = l3Pattern;
+    }
+}
